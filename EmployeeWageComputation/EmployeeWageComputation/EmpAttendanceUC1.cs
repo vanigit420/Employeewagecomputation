3974ecee8e07179aa6402e5383bffcaa2cdbea20
shivani korwan <shivanikorwan63232@gmail.com>
2022-05-31 10:07:59 +0530

    check employee is present or absent

---
+++ EmployeeWageComputation/EmployeeWageComputation/EmpAttendanceUC1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_wage_computation
+{
+    internal class EmployeeWage
+    {
+        public void StartWageCal()
+        {
+            Random random = new Random();
+            const int present = 1;
+            const int absent = 0;
+
+
+            int ispresent = random.Next(2);
+
+            if (ispresent == present)
+            {
+                Console.WriteLine(" Employee is Present ");
+
+            }
+            else
+            {
+                Console.WriteLine(" Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Employee_wage_computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage empWage = new EmployeeWage();
+            empWage.StartWageCal();
+        }
+
+    }
+
+}
